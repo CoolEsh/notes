b9d0937202453e7613c1b416ed5f887c3f9c1847
coolesh <coolesh84@gmail.com>
2014-03-19 12:13:07 +0200

    [+] clean-up

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -3,16 +3,6 @@
 class IndexController extends My_Controller_Action_Abstract
 {
     public function indexAction()
-    {
-        $this->view->page = (int) $this->_getParam( 'page', 1 );
-
-        $this->view->headTitle( 'Notes List' );
-
-        /** @var \Models\Reminder $model */
-        $model = $this->container['modelRepository']->getReminderModel();
-        $paginator = $model->getPageRecords( $this->view->page );
-
-        $this->view->remindersPaginator = $paginator;
-    }
+    {}
 
 }
--- application/modules/default/controllers/NoteController.php
+++ application/modules/default/controllers/NoteController.php
@@ -3,7 +3,17 @@
 class NoteController extends My_Controller_Action_Abstract
 {
     public function indexAction()
-    {}
+    {
+        $this->view->page = (int) $this->_getParam( 'page', 1 );
+
+        $this->view->headTitle( 'Notes List' );
+
+        /** @var \Models\Reminder $model */
+        $model = $this->container['modelRepository']->getReminderModel();
+        $paginator = $model->getPageRecords( $this->view->page );
+
+        $this->view->remindersPaginator = $paginator;
+    }
 
     public function addTextAction()
     {
