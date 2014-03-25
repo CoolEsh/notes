abd7c93df6d99e30e312c4138699c1ae6caf97f8
Andrew Kuleshov <coolesh84@gmail.com>
2014-03-25 11:48:51 +0200

    [+] unit tests

--- library/My/Exceptions/ReminderTodoBasicException.php
+++ library/My/Exceptions/ReminderTodoBasicException.php
@@ -1,4 +1,9 @@
 <?php
 
 class My_Exceptions_ReminderTodoBasicException extends Zend_Exception
-{}
+{
+    public function __construct( $msg = '', $code = 0, Exception $previous = null )
+    {
+        parent::__construct( $msg, 0, $previous );
+    }
+}
