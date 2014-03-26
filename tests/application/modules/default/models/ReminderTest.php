b1a183bd1a2e36bb42d56659ec69c0929ff914c9
coolesh <coolesh84@gmail.com>
2014-03-26 18:12:37 +0200

    [+] unit tests for models begin

--- tests/application/modules/default/models/ReminderTest.php
+++ tests/application/modules/default/models/ReminderTest.php
@@ -5,7 +5,6 @@ class ReminderTextTest extends DbTestCase
     public function __construct()
     {
         $this->_modelClass = 'Reminder';
-        $this->_tableName = 'reminder';
     }
 
     public function testAddTextReminder()
@@ -14,15 +13,17 @@ class ReminderTextTest extends DbTestCase
 
         $model = $this->getContainer()['modelRepository']->getReminderTextModel();
         $model->save( array(
-            'type' => $vDataSet->getValue( $this->_tableName, 0, 'type' ),
-            'title' => $vDataSet->getValue( $this->_tableName, 0, 'title' ),
-            'image' => $vDataSet->getValue( $this->_tableName, 0, 'image' ),
-            'content' => $vDataSet->getValue( $this->_tableName, 0, 'content' )
+            'type' => $vDataSet->getValue( 'reminder', 0, 'type' ),
+            'title' => $vDataSet->getValue( 'reminder', 0, 'title' ),
+            'image' => $vDataSet->getValue( 'reminder', 0, 'image' ),
+            'content' => $vDataSet->getValue( 'reminder_text', 0, 'content' )
         ) );
 
         $vExpected = $this->createXmlDataSet( $this->_filesDir . 'addTextReminder.xml' );
         $vActual = new PHPUnit_Extensions_Database_DataSet_QueryDataSet( $this->getConnection() );
-        $vActual->addTable( $this->_tableName );
+        $vActual->addTable( 'reminder' );
+        $vActual->addTable( 'reminder_text' );
+
         $this->assertDataSetsEqual( $vExpected, $vActual );
     }
 
